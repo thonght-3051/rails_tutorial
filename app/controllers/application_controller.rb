fd8aecad67a23f940f9d1d85627681f0eadbe6f4
thonght-3051 <hoang.trung.thong@sun-asterisk.com>
2022-07-27 16:14:41 +0700

    Chapter 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    locale_avaiable = I18n.available_locales.include?(locale)
+    I18n.locale = locale_avaiable ? locale : I18n.default_locale
+  end
+
+  private
+
+  before_action :set_locale
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  def find_object model, id
+    model.find(id)
+  rescue StandardError
+    render file: Rails.root.to_s << ("/public/404.html")
+  end
 end
